b07488e9209d8671e1a47d691b5f884e77c5f5d9
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-07 11:14:24 -0300

    Criação do Models Funcionario

--- accounts/models.py
+++ accounts/models.py
@@ -2,3 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Funcionario(User):  
+    CARGO = (
+        ('F', 'Funcionário'),
+        ('S', 'Secretário'),
+        ('G', 'Gestor')
+    )
+    
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.DateField(default='2000-02-02')
+    numero = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11)
+    genero = models.CharField(max_length=20)
+    estado_civil = models.CharField(max_length=20)
+    secretaria_trabalho = None
+    cargo = models.CharField(max_length=20, choices=CARGO, default='F')
