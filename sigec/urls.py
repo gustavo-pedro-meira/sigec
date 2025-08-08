063880a3cd5c563f67f10a74f2c5da8fbecbb8ac
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-08 20:57:36 -0300

    URL de token fechada com /

--- sigec/urls.py
+++ sigec/urls.py
@@ -29,8 +29,8 @@ router.register('funcionarios', FuncionarioViewSets)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     # URLS das APP
     path('api/', include(router.urls))
