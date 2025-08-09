646c15bad9ce5979a26959600b298bb2f03d9e9f
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-09 00:03:14 -0300

    Validações Personalizada para Password

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,12 +1,49 @@
 from rest_framework import serializers
 from accounts.models import Funcionario
 from django.contrib.auth.models import User
+import re
 
 class FuncionarioSerializers(serializers.ModelSerializer):
     password = serializers.CharField(write_only = True, style = {'input_type': 'password'})
     class Meta:
         model = Funcionario
         fields = ['id', 'cargo', 'email', 'username', 'password']
+        
+    # Validações Personalizadas
+    def SenhaCaracteresEspeciaisValidation(self, data):
+        caracteres = ['!', '#', '$', '%', '@', '^', '*', '&']
+        if not any(c in data['password'] for c in caracteres):
+            raise serializers.ValidationError('A senha deve conter pelo menos 1 caractere especial.')
+        return data
+    
+    def Senha8DigitosValidation(self, data):
+        if len(data['password']) < 8:
+            raise serializers.ValidationError('A senha deve conter pelo menos 8 caracteres.')
+        return data
+    
+    def SenhaComNumeroValidation(self, data):
+        if not re.search(r'[0-9]', data['password']):
+            raise serializers.ValidationError('A senha deve conter pelo menos 1 número.')
+        return data
+    
+    def SenhaLetraMaiusculaValidation(self, data):
+        if not re.search(r'[A-Z]', data['password']):
+            raise serializers.ValidationError('A senha deve conter pelo 1 letra maiúscula.')
+        return data
+        
+        
+    def SenhaSemEspacoValidation(self, data):
+        if " " in data['password']:
+            raise serializers.ValidationError("A senha não dever conter espaço.")
+        return data
+    
+    def validate(self, data):
+        self.SenhaCaracteresEspeciaisValidation(data)
+        self.Senha8DigitosValidation(data)
+        self.SenhaComNumeroValidation(data)
+        self.SenhaLetraMaiusculaValidation(data)
+        self.SenhaSemEspacoValidation(data)
+        return data
         
     def create(self, validated_data):
         password = validated_data.pop("password")
