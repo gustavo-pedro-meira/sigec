af2f6ab6d1bd1d4e14efdecb4d3af9c285f93fa6
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-13 10:21:23 -0300

    Soft-Delete nos Modelos

--- registry/models.py
+++ registry/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Secretaria(models.Model):
+class BaseModelQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(deleted_at=timezone.now , is_active=False)
+
+class BaseManager(models.Manager):
+    def get_queryset (self):
+        return BaseModelQuerySet( self.model, using=self._db).filter( deleted_at__isnull =True, is_active=True)
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField( auto_now_add =True)
+    updated_at = models.DateTimeField( auto_now=True)
+    deleted_at = models.DateTimeField( editable=False, blank=True, null=True)
+    is_active = models.BooleanField( editable=False, default=True)
+    
+    objects = BaseManager()
+    
+    class Meta:
+        abstract = True
+    
+    def delete(self, **kwargs):
+        self.is_active = False
+        self.deleted_at = timezone.now()
+        self.save()
+        
+    def hard_delete (self, **kwargs):
+        super(BaseModel, self).delete(**kwargs)
+
+class Secretaria(BaseModel):
     nome_secretaria = models.CharField(max_length=200)
     setor_secretaria = models.CharField(max_length=100)
     
@@ -9,7 +37,7 @@ class Secretaria(models.Model):
         return self.nome_secretaria
     
 
-class Cargo(models.Model):
+class Cargo(BaseModel):
     nome_cargo = models.CharField(max_length=150)
     secretaria = models.ForeignKey(Secretaria, on_delete=models.CASCADE)
     
--- tasks/models.py
+++ tasks/models.py
@@ -2,9 +2,37 @@ from django.db import models
 from django.utils import timezone
 from registry.models import Secretaria, Cargo
 from accounts.models import Funcionario
+from django.utils import timezone
 
 # Create your models here.
-class Tarefa(models.Model):
+class BaseModelQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(deleted_at=timezone.now , is_active=False)
+
+class BaseManager(models.Manager):
+    def get_queryset (self):
+        return BaseModelQuerySet( self.model, using=self._db).filter( deleted_at__isnull =True, is_active=True)
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField( auto_now_add =True)
+    updated_at = models.DateTimeField( auto_now=True)
+    deleted_at = models.DateTimeField( editable=False, blank=True, null=True)
+    is_active = models.BooleanField( editable=False, default=True)
+    
+    objects = BaseManager()
+    
+    class Meta:
+        abstract = True
+    
+    def delete(self, **kwargs):
+        self.is_active = False
+        self.deleted_at = timezone.now()
+        self.save()
+        
+    def hard_delete (self, **kwargs):
+        super(BaseModel, self).delete(**kwargs)
+
+class Tarefa(BaseModel):
     class StatusTarefa(models.TextChoices):
         A_FAZER = 'AF', 'A Fazer'
         EM_ANDAMENTO = 'A', 'Andamento'
