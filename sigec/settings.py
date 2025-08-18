fe98de10e3136511c1900b7eed44fc6de8e1f71f
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-18 14:53:56 -0300

    URL Corrigida

--- sigec/settings.py
+++ sigec/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://sigec-9ven.onrender.com"]
+ALLOWED_HOSTS = ["sigec-9ven.onrender.com"]
 
 
 # Application definition
