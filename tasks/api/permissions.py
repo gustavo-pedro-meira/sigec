7004ba301195b53736b5eb0303df0eed1fabd1d6
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-08-31 15:42:44 -0300

    Permissão has_object_permission removida temporariamente

--- tasks/api/permissions.py
+++ tasks/api/permissions.py
@@ -11,12 +11,12 @@ class CriarTarefaPermissions(permissions.BasePermission):
         
         return request.user.funcionario.cargo in ['G', 'S']
     
-    def has_object_permission(self, request, view, obj):
-        if request.user.funcionario.cargo == 'G':
-            return True
+    # def has_object_permission(self, request, view, obj):
+    #     if request.user.funcionario.cargo == 'G':
+    #         return True
         
-        if request.method in permissions.SAFE_METHODS:
-            return True
+    #     if request.method in permissions.SAFE_METHODS:
+    #         return True
         
-        self.message = 'Você só pode alterar ou deletar tarefas que você mesmo criou.'
-        return obj.id_criador == request.user.funcionario
+    #     self.message = 'Você só pode alterar ou deletar tarefas que você mesmo criou.'
+    #     return obj.id_criador == request.user.funcionario
